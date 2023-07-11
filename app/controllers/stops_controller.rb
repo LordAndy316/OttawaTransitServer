2802c90c89c8a3313254ac550a01ba2eb7321bdd
Andrew Henderson <lordAndy316@gmail.com>
2023-07-10 20:21:05 -0400

    Simple database created

--- app/controllers/stops_controller.rb
+++ app/controllers/stops_controller.rb
@@ -6,4 +6,28 @@ class StopsController < ApplicationController
   def show 
     @stop = Stop.find(params[:id])
   end
+    def new
+    @stop = Stop.new
+  end
+
+  def create
+    @stop = Stop.new(stop_params)
+
+    if @stop.save
+      redirect_to @stop
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  def destroy
+    @stop = Stop.find(params[:id])
+    @stop.destroy
+
+    redirect_to root_path, status: :see_other
+  end
+
+  private
+    def stop_params
+      params.require(:stop).permit(:title, :body)
+    end
 end
--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,2 +1,4 @@
 class Stop < ApplicationRecord
+    validates :StopNumber, presence: true
+    validates :StopDescription, presence: true, length: { minimum: 10 }
 end
